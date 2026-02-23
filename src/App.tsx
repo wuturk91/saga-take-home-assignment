bd7a32c4960a233ed62ec76b516b8ff5986fe12a
wuturk91 <cemsalih91@gmail.com>
2026-02-23 15:56:25 +0000

    Fix CORS issue with vite proxy config; Render basic elements from results returned from useProducts hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useProducts } from './hooks/useProducts'
 import './App.css'
+import type { Product } from './types'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { results, loading } = useProducts()
+
+  if (loading) return <p>Loading products...</p>
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Holidays</h1>
+      {results.map((result: Product) => (
+        <div key={result.id}>
+          <img src={result.images[0].filename} alt={result.images[0].alt} />
+          <p>{result.name}</p>
+          <p>Total Price: £{result.departure.groupPrice.price}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 

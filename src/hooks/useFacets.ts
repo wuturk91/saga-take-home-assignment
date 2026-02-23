6bb65e55a0b7c1873fa033cdc53d0fe97a8a8db7
wuturk91 <cemsalih91@gmail.com>
2026-02-23 16:36:48 +0000

    Update facet state to be of Facet[]

--- src/hooks/useFacets.ts
+++ src/hooks/useFacets.ts
@@ -3,13 +3,13 @@ import { fetchFacets } from '../api/sagaApi'
 import { type Facet } from '../types'
 
 export const useFacets = () => {
-  const [facets, setFacets] = useState<Facet>()
+  const [facets, setFacets] = useState<Facet[]>()
   const [facetsLoading, setFacetsLoading] = useState<boolean>(true)
 
   useEffect(() => {
     fetchFacets()
       .then(result => {
-        const limitedFacets = result.filter((facet: Facet) => facet.key === 'HolidayType' || facet.key === 'ProductType')
+        const limitedFacets: Facet[] = result.filter((facet: Facet) => facet.key === 'HolidayType' || facet.key === 'ProductType')
         setFacets(limitedFacets)
         setFacetsLoading(false)
       })

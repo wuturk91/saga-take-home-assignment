10a7add1506e4407b3a3f2a56ce8e15bc20a07db
wuturk91 <cemsalih91@gmail.com>
2026-02-23 16:14:57 +0000

    Update api utility with facet fetch; Create useFacets hook which limits the return to either Holiday or ProductType facets

--- src/api/sagaApi.ts
+++ src/api/sagaApi.ts
@@ -10,3 +10,16 @@ export const fetchProducts = async () => {
     // handle error with log and return error message
   }
 }
+
+export const fetchFacets = async () => {
+  const url: string = 'api/search/facets'
+
+  try {
+    const response: Response = await fetch(url)
+    if (!response.ok) throw new Error('Failed to fetch facets')
+    const { facets } = await response.json()
+    return facets
+  } catch (error) {
+    // handle error with log and return error message
+  }
+}
---
+++ src/hooks/useFacets.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+import { fetchFacets } from '../api/sagaApi'
+import { type Facet } from '../types'
+
+export const useFacets = () => {
+  const [facets, setFacets] = useState<Facet>()
+  const [facetsLoading, setFacetsLoading] = useState<boolean>(true)
+
+  useEffect(() => {
+    fetchFacets()
+      .then(result => {
+        const limitedFacets = result.filter((facet: Facet) => facet.key === 'HolidayType' || facet.key === 'ProductType')
+        setFacets(limitedFacets)
+        setFacetsLoading(false)
+      })
+  }, [])
+
+  return { facets, facetsLoading }
+}
